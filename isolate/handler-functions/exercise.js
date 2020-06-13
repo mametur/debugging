4323880b6b12f2cec5fab1c7dd484bd1bcde4544
Mamé <mameazad.turgut@gmail.com>
2020-06-13 20:34:12 +0200

    solve handler exercises

--- isolate/handler-functions/exercise.js
+++ isolate/handler-functions/exercise.js
@@ -8,25 +8,29 @@
 */
 
 function handleUserInput() {
-  'use strict';
+	'use strict';
 
-  // read user input
-  _;
-  _;
-  _;
+	// read user input
+	let user_1_input = prompt('fill languages that you speak', 'eng');
+	let user_2_input = prompt('fill languages that you speak', 'du');
+	let user_3_input = prompt('fill languages that you speak', 'fr');
 
-  // execute program logic
-  _;
+	// execute program logic
+	let show_user_input = `I speak ${user_1_input}, ${user_2_input} and ${user_3_input}`;
+	console.log(show_user_input);
+	// communicate the result to the user
+	alert(show_user_input);
 
-  // communicate the result to the user
-  _;
-
-  // log the action for developers
-  console.log('-- handled input --');
-  _;
-  _;
-  _;
-  _;
+	// log the action for developers
+	console.log('-- handled input --');
+	console.log('user input 1; ', typeof user_1_input, user_1_input);
+	console.log('user input 2; ', typeof user_2_input, user_2_input);
+	console.log('user input 3; ', typeof user_3_input, user_3_input);
+	console.log(
+		'show user whole message; ',
+		typeof show_user_input,
+		show_user_input
+	);
 }
 
 handleUserInput();
