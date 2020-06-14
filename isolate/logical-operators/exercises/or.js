b62116ca153f976a12f4ed019f2830662296bb05
Mamé <mameazad.turgut@gmail.com>
2020-06-14 11:14:01 +0200

    solve or.js

--- isolate/logical-operators/exercises/or.js
+++ isolate/logical-operators/exercises/or.js
@@ -3,7 +3,11 @@
 // write a function that behaves the same as ||
 //  ... without using ||
 function or(left, right) {
-
+	if (left) {
+		return left;
+	} else {
+		return right;
+	}
 }
 
 const or1 = or(0, 0);
