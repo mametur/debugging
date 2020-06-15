7ceddecb7d4ecd826008406d1af598e7601fd474
Mamé <mameazad.turgut@gmail.com>
2020-06-15 15:04:46 +0200

    solve 1.js and 2.js but not sure last two solution of 2.js

--- isolate/explicit-coercion/exercises/1.js
+++ isolate/explicit-coercion/exercises/1.js
@@ -1,48 +1,49 @@
 'use strict';
-
+debugger;
 function mystery(x) {
-  if (typeof x === 'boolean') { throw new TypeError('x cannot be a boolean'); }
+	if (typeof x === 'boolean') {
+		throw new TypeError('x cannot be a boolean');
+	}
 
-  const booleaned = Boolean(x);
-  const numbered = Number(booleaned);
-  return numbered;
+	const booleaned = Boolean(x);
+	const numbered = Number(booleaned);
+	return numbered;
 }
 
 // write the expected return value
 
 const returned1 = mystery(undefined);
-const isTrue1 = returned1 === _;
+const isTrue1 = returned1 === 0;
 console.assert(isTrue1, 'Test 1');
 
 const returned2 = mystery(null);
-const isTrue2 = returned2 === _;
+const isTrue2 = returned2 === 0;
 console.assert(isTrue2, 'Test 2');
 
 const returned3 = mystery('false');
-const isTrue3 = returned3 === _;
+const isTrue3 = returned3 === 1;
 console.assert(isTrue3, 'Test 3');
 
-const returned4 = mystery('');
-const isTrue4 = returned4 === _;
+const returned4 = mystery(''); // Number(0,-0, NaN, Undefined, "")->False   Number(false)>0
+const isTrue4 = returned4 === 0;
 console.assert(isTrue4, 'Test 4');
 
 const returned5 = mystery(0);
-const isTrue5 = returned5 === _;
+const isTrue5 = returned5 === 0;
 console.assert(isTrue5, 'Test 5');
 
-const returned6 = mystery('  ');
-const isTrue6 = returned6 === _;
+const returned6 = mystery('  '); // Number('')-> 0 Number('  ')-> 0
+const isTrue6 = returned6 === 1;
 console.assert(isTrue6, 'Test 6');
 
 const returned7 = mystery(-1);
-const isTrue7 = returned7 === _;
+const isTrue7 = returned7 === 1;
 console.assert(isTrue7, 'Test 7');
 
 const returned8 = mystery(true);
-const isTrue8 = returned8 === _;
+const isTrue8 = returned8 === 'x cannot be a boolean';
 console.assert(isTrue8, 'Test 8');
 
 const returned9 = mystery(1);
-const isTrue9 = returned9 === _;
+const isTrue9 = returned9 === 1;
 console.assert(isTrue9, 'Test 9');
-
--- isolate/explicit-coercion/exercises/2.js
+++ isolate/explicit-coercion/exercises/2.js
@@ -1,48 +1,49 @@
 'use strict';
 
 function mystery(x) {
-  if (typeof x === 'string') { throw new TypeError('x cannot be a string'); }
+	if (typeof x === 'string') {
+		throw new TypeError('x cannot be a string');
+	}
 
-  const stringed = String(x);
-  const numbered = Number(stringed);
-  return numbered;
+	const stringed = String(x);
+	const numbered = Number(stringed);
+	return numbered;
 }
 
 // find an argument to get the expected return value
 
-const returned1 = mystery(_);
-const isTrue1 = returned1 === -1;
+const returned1 = mystery(-1);
+const isTrue1 = returned1 === -1; //'-1'> -1
 console.assert(isTrue1, 'Test 1');
 
-const returned2 = mystery(_);
+const returned2 = mystery(0);
 const isTrue2 = returned2 === 0;
 console.assert(isTrue2, 'Test 2');
 
-const returned3 = mystery(_);
+const returned3 = mystery(1);
 const isTrue3 = returned3 === 1;
 console.assert(isTrue3, 'Test 3');
 
-const returned4 = mystery(_);
+const returned4 = mystery(0.5);
 const isTrue4 = returned4 === 0.5;
 console.assert(isTrue4, 'Test 4');
 
-const returned5 = mystery(_);
+const returned5 = mystery(1e3);
 const isTrue5 = returned5 === 1000;
 console.assert(isTrue5, 'Test 5');
 
-const returned6 = mystery(_);
+const returned6 = mystery(0 / 0); // NaN or 0/0
 const isTrue6 = Number.isNaN(returned6);
 console.assert(isTrue6, 'Test 6');
 
-const returned7 = mystery(_);
+const returned7 = mystery(NaN);
 const isTrue7 = Number.isNaN(returned7);
 console.assert(isTrue7, 'Test 7');
 
-const returned8 = mystery(_);
+const returned8 = mystery({});
 const isTrue8 = Number.isNaN(returned8);
 console.assert(isTrue8, 'Test 8');
-
-const returned9 = mystery(_);
+// I know just two ways for Number.isNaN()-> true
+const returned9 = mystery(Number.NaN);
 const isTrue9 = Number.isNaN(returned9);
 console.assert(isTrue9, 'Test 9');
-
